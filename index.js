4c2777e8e5d5effde3d5786bbbf5f0fc0d7bd5f4
Maksym Habinskyi <mgabin348@gmail.com>
2025-08-08 23:37:03 +0200

    added api/whoami route that returns client information

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 import express from "express";
 
-const app = express();	
-const PORT = process.env.PORT || 3000; 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.get("/api/whoami", (req, res) => {
+  const ipadress =
+    req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  const language = req.headers["accept-language"]
+    ? req.headers["accept-language"].split(",")[0]
+    : "unknown";
+  const software = req.headers["user-agent"]
+    ? req.headers["user-agent"]
+    : "unknown";
+  res.json({ ipadress, language, software });
+});
 
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
