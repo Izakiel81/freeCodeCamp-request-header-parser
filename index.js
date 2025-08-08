0b5dbd7db903fe4683434246d910ffcee8b3b53d
Maksym Habinskyi <mgabin348@gmail.com>
2025-08-08 23:44:25 +0200

    fixed spelling

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/api/whoami", (req, res) => {
-  const ipadress =
+  const ipaddress =
     req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   const language = req.headers["accept-language"]
     ? req.headers["accept-language"].split(",")[0]
@@ -12,7 +12,7 @@ app.get("/api/whoami", (req, res) => {
   const software = req.headers["user-agent"]
     ? req.headers["user-agent"]
     : "unknown";
-  res.json({ ipadress, language, software });
+  res.json({ ipaddress, language, software });
 });
 
 app.listen(PORT, () => {
